8392910b81b1c7ec736efaf30e8a8750e3c03e8e
rrozb <rrozbeiko@gmail.com>
2022-07-31 12:25:02 +0200

    added bb strategy

--- library/basic_alogs.py
+++ library/basic_alogs.py
@@ -1,5 +1,6 @@
 from pylgo.algorithm import AlgorithmBase
 from pylgo.alpha import Signal, SignalType
+from ta.volatility import BollingerBands
 
 
 class BuyLiquidateAlgo(AlgorithmBase):
@@ -50,4 +51,54 @@ class SMAAlgo(AlgorithmBase):
                     yield Signal(SignalType.LIQUIDATE, symbol)
                 elif position_type is SignalType.SELL and is_above:
                     yield Signal(SignalType.LIQUIDATE, symbol)
+        return signals
+
+
+class BBAlgo(AlgorithmBase):
+    '''
+    Boilinger bandas Long/Short algorithm.
+    '''
+    algo_name = 'BB'
+    window = 30
+    deviation = 2.5
+
+    def create_signals(self, current_data):
+        signals = []
+        for symbol, snapshot in current_data.items():
+            symbol_data = snapshot.data
+            if len(symbol_data) < self.window:
+                continue
+            indicator_bb = BollingerBands(
+                close=symbol_data["close"], window=self.window, window_dev=self.deviation)
+            symbol_data = symbol_data.copy()
+            # Add Bollinger Bands features
+            symbol_data['bb_bbm'] = list(indicator_bb.bollinger_mavg().values)
+            symbol_data['bb_bbh'] = list(indicator_bb.bollinger_hband().values)
+            symbol_data['bb_bbl'] = list(indicator_bb.bollinger_lband().values)
+
+            # Add Bollinger Band high indicator
+            symbol_data['bb_bbhi'] = list(
+                indicator_bb.bollinger_hband_indicator().values)
+
+            # Add Bollinger Band low indicator
+            symbol_data['bb_bbli'] = list(
+                indicator_bb.bollinger_lband_indicator().values)
+            symbol_position = self.portfolio.positions.get_symbol_position(
+                symbol)
+            is_buy = symbol_data['bb_bbli'].iloc[-1] == 1
+            is_sell = symbol_data['bb_bbhi'].iloc[-1] == 1
+            if symbol_position is None and is_buy:
+                yield Signal(SignalType.BUY, symbol)
+            elif symbol_position is None and not is_sell:
+                yield Signal(SignalType.SELL, symbol)
+
+            if symbol_position is not None:
+                position_type = symbol_position.signal.signal_type
+                below_mavg = symbol_data['close'].iloc[-1] <= symbol_data['bb_bbm'].iloc[-1]
+                above_mavg = symbol_data['close'].iloc[-1] >= symbol_data['bb_bbm'].iloc[-1]
+                if position_type is SignalType.BUY and not above_mavg:
+                    yield Signal(SignalType.LIQUIDATE, symbol)
+                elif position_type is SignalType.SELL and below_mavg:
+                    yield Signal(SignalType.LIQUIDATE, symbol)
+
         return signals
