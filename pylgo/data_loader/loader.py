f5640d3b296f156241bbc9832f1d9b11c9d0d343
rrozb <rrozbeiko@gmail.com>
2022-08-21 13:10:45 +0200

    merged

--- pylgo/data_loader/loader.py
+++ pylgo/data_loader/loader.py
@@ -1,13 +1,14 @@
 import datetime
 
 from engine.db import CandlesRepo
-#import pandas as pd
+import pandas as pd
 
 from .data_model import History, HistoryCollection
 import requests
 import pytz
 import time
 from datetime import datetime
+
 
 class Loader:
     '''
@@ -16,9 +17,9 @@ class Loader:
 
     def __init__(self, symbols, frequency, start=None, end=None, prefix='Bitfinex',) -> None:
         self.symbols = symbols
-        self.start = datetime.datetime.strptime(
+        self.start = datetime.strptime(
             start, '%Y-%m-%d %H:%M:%S') if start else None
-        self.end = datetime.datetime.strptime(
+        self.end = datetime.strptime(
             end, '%Y-%m-%d %H:%M:%S') if end else None
         self.frequency = frequency
         self.prefix = prefix
@@ -43,9 +44,10 @@ class Loader:
         except Exception as e:
             raise Exception(e)
 
-#TODO Add timeframe to BitFInexLoaderAPI
+
+# TODO Add timeframe to BitFInexLoaderAPI
 TIMEFRAME = {
-    "1m":"60000",
+    "1m": "60000",
     "5m": "300000",
     '15m': "900000",
     '30m': '1800000',
@@ -59,10 +61,13 @@ TIMEFRAME = {
     '1M': '2592000000'
 
 }
+
+
 class BitfinexLoaderAPI():
     """
     Class to work with Bitfinex API
     """
+
     def __init__(self, symbol: str, start_date: datetime, end_date: datetime, timeframe: str = None, **kwargs) -> None:
         self.baseUrl = "https://api-pub.bitfinex.com/v2/candles/trade:1m:"
         self.symbol = symbol
@@ -72,7 +77,7 @@ class BitfinexLoaderAPI():
             end_date, "%Y-%m-%d %H:%M:%S").replace(tzinfo=pytz.UTC)
         self.timestep = 10000 * 60000
         self.start_date_unix = self.start_date.timestamp() * 1000
-        self.end_date_unix = self.end_date.timestamp() * 1000      
+        self.end_date_unix = self.end_date.timestamp() * 1000
 
     def get_data(self) -> None:
         """
@@ -81,23 +86,25 @@ class BitfinexLoaderAPI():
         current_time = self.start_date_unix
         while current_time < self.end_date_unix:
             if self.end_date_unix - current_time > self.timestep:
-                self.send_request(start=current_time, end=current_time+self.timestep)
+                self.send_request(start=current_time,
+                                  end=current_time+self.timestep)
                 current_time += self.timestep
             else:
                 self.send_request(start=current_time, end=self.end_date_unix)
-                current_time = (self.end_date_unix - current_time) + current_time
+                current_time = (self.end_date_unix -
+                                current_time) + current_time
 
     def send_request(self, start: int, end: int) -> None:
         """
         Send request to Bitfinex API with current date
         """
-        r = requests.get(f'{self.baseUrl}{self.symbol}/hist?start={start}&end={end}&limit=10000')
+        r = requests.get(
+            f'{self.baseUrl}{self.symbol}/hist?start={start}&end={end}&limit=10000')
         if r.status_code == 200:
             for candle in r.json():
-                CandlesRepo.create(candles=candle, symbol=self.symbol)           
+                CandlesRepo.create(candles=candle, symbol=self.symbol)
         if r.status_code == 429:
             print('You have reached ratelimit. Waiting 1 minute...')
             time.sleep(60)
-            r = requests.get(f'{self.baseUrl}{self.symbol}/hist?start={start}&end={end}&limit=10000')
-
-
+            r = requests.get(
+                f'{self.baseUrl}{self.symbol}/hist?start={start}&end={end}&limit=10000')
--- test_algo.py
+++ test_algo.py
@@ -1,8 +1,8 @@
-#from library import BuyLiquidateAlgo, SMAAlgo
-from pylgo.data_loader.loader import BitfinexLoaderAPI
-# algorithm = SMAAlgo(['BTCUSD', 'ETHUSD'], 'Bitfinex', '1h',
-#                    '2021-12-01 06:00:00', '2022-01-01 10:00:00', 'logs', 'reports', 10_000)
-# algorithm.run()
-s = BitfinexLoaderAPI(
-    symbol='tBTCUSD', start_date="2022-01-01 00:00:00", end_date="2022-07-07 00:10:00")
-s.get_data()
+from library import LSTMAlgo
+# from pylgo.data_loader.loader import BitfinexLoaderAPI
+algorithm = LSTMAlgo(['BTCUSD'], 'Bitfinex', 'd',
+                     '2022-03-11 00:00:00', '2022-08-20 00:00:00', 'logs', 'reports', 10_000)
+algorithm.run()
+# s = BitfinexLoaderAPI(
+#     symbol='tBTCUSD', start_date="2022-01-01 00:00:00", end_date="2022-07-07 00:10:00")
+# s.get_data()
